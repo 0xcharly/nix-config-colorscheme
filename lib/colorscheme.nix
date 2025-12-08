740e8173dacf209f4d77e11f916d04212fbf0315
Charly Delay <charly@delay.gg>
2025-12-08 18:26:29 +0900

    No-op cleanup

--- lib/colorscheme.nix
+++ lib/colorscheme.nix
@@ -1,10 +1,7 @@
 lib: oklch: rgb: theme: let
   mapHexAttrs = fn:
     lib.attrsets.mapAttrs (_: value: fn (rgb.toHex (oklch.convertToRgb value)));
-
-  mapToHexLiterals = mapHexAttrs (value: "0x${value}");
-  mapToHexStrings = mapHexAttrs (value: "#${value}");
 in {
-  asHexLiterals = mapToHexLiterals theme;
-  asHexStrings = mapToHexStrings theme;
+  asHexLiterals = mapHexAttrs (value: "0x${value}") theme;
+  asHexStrings = mapHexAttrs (value: "#${value}") theme;
 }
