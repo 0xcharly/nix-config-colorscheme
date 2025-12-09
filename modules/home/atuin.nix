94127ec4fed78f015ba132e77cc419d7b5a3159a
Charly Delay <charly@delay.gg>
2025-12-09 14:22:16 +0900

    Add atuin colorscheme

---
+++ modules/home/atuin.nix
@@ -0,0 +1,19 @@
+{flake, ...}: let
+  theme_name = "pixel";
+  theme = flake.lib.colorscheme.hasHexStrings;
+in {
+  programs.atuin.settings.theme.name = theme_name;
+  xdg.configFile."atuin/themes/${theme_name}.toml".text = builtins.toTOML {
+    theme.name = theme_name;
+    colors = {
+      AlertError = theme.text_error;
+      AlertInfo = theme.text_ok;
+      AlertWarn = theme.text_warning;
+      Annotation = theme.text_info;
+      Base = theme.text;
+      Guidance = theme.text_comment;
+      Important = theme.text_red;
+      Title = theme.text_title;
+    };
+  };
+}
