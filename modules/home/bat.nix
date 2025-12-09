8cc147aae7f6d0c5f04cafe776fa44a3412f9cd7
Charly Delay <charly@delay.gg>
2025-12-09 21:28:18 +0900

    Add bat colorscheme (only an import of the catppuccin one for now; needs refactoring with nvim colorscheme)

---
+++ modules/home/bat.nix
@@ -0,0 +1,9 @@
+{flake, ...}: {pkgs, ...}: let
+  theme_name = "pixel";
+  theme = flake.lib.colorscheme.asHexStrings;
+in {
+  programs.bat = {
+    config.theme = theme_name;
+    themes.${theme_name} = ./pixel.tmTheme;
+  };
+}
