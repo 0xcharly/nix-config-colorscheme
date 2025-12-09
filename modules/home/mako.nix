fc98d1604a686324b596d8089b9dc5590ad24af1
Charly Delay <charly@delay.gg>
2025-12-09 14:11:38 +0900

    mako: simplify implementation

--- modules/home/mako.nix
+++ modules/home/mako.nix
@@ -1,13 +1,12 @@
-{flake, ...}: {pkgs, ...}: let
+{flake, ...}: let
   theme = flake.lib.colorscheme.hasHexStrings;
 in {
-  config.services.mako.settings.include = pkgs.writeText "pixel.theme" ''
-    background-color=${theme.surface}
-    text-color=${theme.text}
-    border-color=${theme.text_blue}
-    progress-color=over ${theme.surface_cursorline}
+  config.services.mako.settings = {
+    background-color = theme.surface;
+    text-color = theme.text;
+    border-color = theme.text_blue;
+    progress-color = theme.surface_cursorline;
 
-    [urgency=high]
-    border-color=${theme.text_red}
-  '';
+    "urgency=high".border-color = theme.text_red;
+  };
 }
