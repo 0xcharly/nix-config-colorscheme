9389b3674801692b0fb26c3a0e359e0ec02db7ab
Charly Delay <charly@delay.gg>
2025-12-10 00:20:17 +0900

    Add tmux colorscheme

---
+++ modules/home/tmux.nix
@@ -0,0 +1,12 @@
+{flake, ...}: {lib, ...}: let
+  theme = flake.lib.colorscheme.noPrefix;
+in {
+  programs.tmux.extraConfig = lib.mkBefore ''
+    set -ogq @text "${theme.text}"
+    set -ogq @text_variant_dim "${theme.text_variant_dim}"
+    set -ogq @text_violet "${theme.text_violet}"
+    set -ogq @surface "${theme.surface}"
+    set -ogq @surface_cursorline "${theme.surface_cursorline}"
+    set -ogq @surface_menu "${theme.surface_menu}"
+  '';
+}
