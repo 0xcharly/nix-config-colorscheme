c74bf4f9c3b42de6d89f14a19b295ef25c14fac5
Charly Delay <charly@delay.gg>
2025-12-09 13:53:26 +0900

    Add FZF colorscheme

---
+++ modules/home/fzf.nix
@@ -0,0 +1,23 @@
+{flake, ...}: let
+  theme = flake.lib.colorscheme.hasHexStrings;
+in {
+  programs.fzf.colors = {
+    "bg+" = theme.surface_variant;
+    "fg+" = theme.text_title;
+    "hl+" = theme.text_sky;
+    bg = theme.surface;
+    border = theme.borders;
+    disabled = theme.text_variant_dimmer;
+    fg = theme.text;
+    gutter = theme.surface;
+    header = theme.text_cyan;
+    hl = theme.text_blue;
+    info = theme.text_amber;
+    marker = theme.text_emerald;
+    pointer = theme.text_violet;
+    prompt = theme.text_blue;
+    query = theme.text;
+    separator = theme.borders;
+    spinner = theme.text_violet;
+  };
+}
