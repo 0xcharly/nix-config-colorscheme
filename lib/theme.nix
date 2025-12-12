2fecef81865ed4407cb5ceb5137007fa27e05d12
Charly Delay <charly@delay.gg>
2025-12-12 14:16:44 +0900

    Update accent color from blue to sky

--- lib/theme.nix
+++ lib/theme.nix
@@ -41,10 +41,10 @@ with palette; rec {
   text_info = tailwind.blue-300;
   text_hint = tailwind.indigo-300;
 
-  accent = tailwind.blue-200;
-  accent_dark = tailwind.blue-300;
-  accent_darker = tailwind.blue-400;
-  accent_darkest = tailwind.blue-500;
+  accent = tailwind.sky-200;
+  accent_dark = tailwind.sky-300;
+  accent_darker = tailwind.sky-400;
+  accent_darkest = tailwind.sky-500;
 
   borders = tailwind.slate-500;
 
