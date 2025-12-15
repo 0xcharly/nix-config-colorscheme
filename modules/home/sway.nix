7498f51e4b066f27326abae56c3c4880a7f0346a
Charly Delay <delay@google.com>
2025-12-15 18:52:02 +0900

    Sway: add missing `background` properties

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -8,30 +8,35 @@ in {
         text = theme.borders_active;
         indicator = theme.borders_active;
         childBorder = theme.borders_active;
+        background = theme.surface_active;
       };
       focusedInactive = {
         border = theme.borders_focused_inactive;
         text = theme.borders_focused_inactive;
         indicator = theme.borders_focused_inactive;
         childBorder = theme.borders_focused_inactive;
+        background = theme.surface_focused_inactive;
       };
       unfocused = {
         border = theme.borders_inactive;
         text = theme.borders_inactive;
         indicator = theme.borders_inactive;
         childBorder = theme.borders_inactive;
+        background = theme.surface_inactive;
       };
       urgent = {
         border = theme.borders_urgent;
         text = theme.borders_urgent;
         indicator = theme.borders_urgent;
         childBorder = theme.borders_urgent;
+        background = theme.surface_urgent;
       };
       placeholder = {
         border = theme.borders;
         text = theme.borders;
         indicator = theme.borders;
         childBorder = theme.borders;
+        background = theme.surface_dark;
       };
     };
     extraConfig = ''
