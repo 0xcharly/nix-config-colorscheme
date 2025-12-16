3d0d466fa7ffa4b57827ccf0c15db0d33dec7500
Charly Delay <delay@google.com>
2025-12-16 16:09:03 +0900

    Kitty: fix theme

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,24 +1,29 @@
-{flake, ...}: {lib, ...}: let
-  themeFile = "pixel";
+{flake, ...}: {
+  lib,
+  pkgs,
+  ...
+}: let
   theme = flake.lib.colorscheme.asHexStrings;
 in {
-  xdg.configFile."kitty/themes/${themeFile}.conf".text =
-    ''
-      background ${theme.surface}
-      foreground ${theme.text}
-      selection_background ${theme.surface_visual}
-      selection_foreground ${theme.on_surface_visual}
-      cursor ${theme.text}
-      cursor_text_color ${theme.surface}
-      url_color ${theme.text_link}
+  programs.kitty.extraConfig = let
+    themeFile =
+      pkgs.writeTextFile "pixel.conf" ''
+        background ${theme.surface}
+        foreground ${theme.text}
+        selection_background ${theme.surface_visual}
+        selection_foreground ${theme.on_surface_visual}
+        cursor ${theme.text}
+        cursor_text_color ${theme.surface}
+        url_color ${theme.text_link}
 
-      # OS Window titlebar colors
-      wayland_titlebar_color system
-      macos_titlebar_color system
-    ''
-    + lib.concatStringsSep "\n" (builtins.map (
-      index: "color${toString index} ${theme."terminal_color_${toString index}"}"
-    ) (lib.lists.range 0 15));
-
-  programs.kitty = {inherit themeFile;};
+        # OS Window titlebar colors
+        wayland_titlebar_color system
+        macos_titlebar_color system
+      ''
+      + lib.concatStringsSep "\n" (builtins.map (
+        index: "color${toString index} ${theme."terminal_color_${toString index}"}"
+      ) (lib.lists.range 0 15));
+  in ''
+    include ${themeFile}
+  '';
 }
