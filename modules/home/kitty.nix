5298e3656b9db1ba8eea6ea0ac88c5c6ab37053b
Charly Delay <delay@google.com>
2025-12-16 16:15:04 +0900

    Kitty: fix theme file generator

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -6,8 +6,9 @@
   theme = flake.lib.colorscheme.asHexStrings;
 in {
   programs.kitty.extraConfig = let
-    themeFile =
-      pkgs.writeText "pixel.conf" ''
+    content =
+      ''
+        # Basic colors
         background ${theme.surface}
         foreground ${theme.text}
         selection_background ${theme.surface_visual}
@@ -19,11 +20,13 @@ in {
         # OS Window titlebar colors
         wayland_titlebar_color system
         macos_titlebar_color system
+
+        # 16 terminal colors
       ''
       + lib.concatStringsSep "\n" (builtins.map (
         index: "color${toString index} ${theme."terminal_color_${toString index}"}"
       ) (lib.lists.range 0 15));
   in ''
-    include ${themeFile}
+    include ${pkgs.writeText "pixel.conf" content}
   '';
 }
