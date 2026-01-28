52650b69a16282998c739fc47cead3baf7405146
Charly Delay <charly@delay.gg>
2026-01-28 23:39:33 +0900

    Tune cursor color

--- lib/theme.nix
+++ lib/theme.nix
@@ -45,7 +45,7 @@ with palette; rec {
   text_hint = tailwind.indigo-300;
 
   text_cursor = tailwind.neutral-900;
-  surface_cursor = tailwind.red-600;
+  surface_cursor = tailwind.red-400;
 
   accent = tailwind.sky-200;
   accent_dark = tailwind.sky-300;
