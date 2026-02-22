0b6cc7f219aba0e707de173a6d69a680746df520
Charly Delay <charly@delay.gg>
2026-02-22 12:17:05 +0900

    TMUX: tentatively update colorscheme to match nvim statusline

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -6,11 +6,11 @@ in
 {
   programs.tmux.extraConfig = lib.mkBefore ''
     set -ogq @text "${theme.text}"
-    set -ogq @text_session_name "${theme.text_variant_dim}"
+    set -ogq @text_session_name "${theme.on_surface_statusline_dimmer}"
     set -ogq @surface "${theme.surface}"
-    set -ogq @surface_statusline "${theme.surface_cursorline}"
+    set -ogq @surface_statusline "${theme.surface_statusline}"
     set -ogq @indicator_current "${theme.accent_darkest}"
-    set -ogq @indicator_last "${theme.text_variant_conceal}"
-    set -ogq @indicator_inactive "${theme.surface_cursorline}"
+    set -ogq @indicator_last "${theme.accent}"
+    set -ogq @indicator_inactive "${theme.surface_statusline}"
   '';
 }
