b7671a39d4e3eb03c5fcde5bd8f347937c111107
Charly Delay <charly@delay.gg>
2026-02-22 12:25:40 +0900

    TMUX: minor tweaks to the colorscheme

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -8,7 +8,7 @@ in
     set -ogq @text "${theme.text}"
     set -ogq @text_session_name "${theme.on_surface_statusline}"
     set -ogq @surface "${theme.surface}"
-    set -ogq @surface_statusline "${theme.surface_statusline_dim}"
+    set -ogq @surface_statusline "${theme.surface_statusline}"
     set -ogq @indicator_current "${theme.accent_darkest}"
     set -ogq @indicator_last "${theme.accent_dark}"
     set -ogq @indicator_inactive "${theme.surface_statusline}"
