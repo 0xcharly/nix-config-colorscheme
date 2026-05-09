d4af1d26818ed2dc7a6c1f1d04477104c98d0ddf
Charly Delay <charly@delay.gg>
2026-05-09 22:24:28 +0900

    Update cursor color

--- lib/_theme.nix
+++ lib/_theme.nix
@@ -96,7 +96,7 @@ rec {
   surface_scrollbar = blends.surface_lighter;
   surface_scrollbar_thumb = tailwind.neutral-800;
 
-  surface_cursor = tailwind.amber-600;
+  surface_cursor = tailwind.violet-400;
   on_surface_cursor = tailwind.neutral-900;
 
   surface_visual = tailwind.blue-800;
