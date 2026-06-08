0ad9c98b52138beb02553fdeb45beaab0061cb80
Charly Delay <charly@delay.gg>
2026-06-09 02:23:59 +0900

    hyprland: tentative fix for lua config

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -15,15 +15,15 @@ in
           color_inactive = shadows_inactive;
         };
         misc.background_color = surface_dark;
-        plugin.hy3.tabs = {
-          "col.active" = surface_active;
-          "col.inactive" = surface_inactive;
-          "col.focused" = surface_focused_inactive;
-          "col.urgent" = surface_urgent;
-          "col.active.border" = borders_active;
-          "col.inactive.border" = borders_inactive;
-          "col.focused.border" = borders_focused_inactive;
-          "col.urgent.border" = borders_urgent;
+        plugin.hy3.tabs.colors = {
+          "active" = surface_active;
+          "inactive" = surface_inactive;
+          "focused" = surface_focused_inactive;
+          "urgent" = surface_urgent;
+          "active_border" = borders_active;
+          "inactive_border" = borders_inactive;
+          "focused_border" = borders_focused_inactive;
+          "urgent_border" = borders_urgent;
         };
       };
     };
